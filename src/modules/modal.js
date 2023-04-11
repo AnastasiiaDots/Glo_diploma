f1f4e1f5c371e1d6ea24677195057b0d25e00fa5
Anastasiia Dotsenko <anastasiia_dots@yahoo.com>
2023-04-12 10:43:27 +1200

    modal open-close

---
+++ src/modules/modal.js
@@ -0,0 +1,32 @@
+const modal = () => {
+  const modal = document.querySelector(".modal-callback");
+  const overlay = document.querySelector(".modal-overlay ");
+  const callbackBtn = document.querySelectorAll(".callback-btn");
+
+  callbackBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+      overlay.style.display = "block";
+    });
+  });
+
+  const closeModal = () => {
+    modal.style.display = "none";
+    overlay.style.display = "none";
+  };
+
+  modal.addEventListener("click", (e) => {
+    if (
+      !e.target.closest("body") ||
+      e.target.matches("img[alt='modal-close']")
+    ) {
+      console.log("closed");
+      closeModal();
+    }
+  });
+
+  overlay.addEventListener("click", () => {
+    closeModal();
+  });
+};
+export default modal;
